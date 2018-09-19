14a6fa9cf7891628fc46e1ee65bb3a7e50c497a4
JustNrik <35231903+JustNrik@users.noreply.github.com>
2018-09-19 17:03:30 -0400

    Delete EmptyObjectException.vb

--- Exceptions/EmptyObjectException.vb
+++
@@ -1,7 +0,0 @@
-﻿Public Class EmptyObjectException
-    Inherits Exception
-    Private Const _defaul = "The object doesn't have any property with Priority Attribute"
-    Sub New()
-        MyBase.New(_defaul)
-    End Sub
-End Class
