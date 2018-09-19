0a78b0b8c91123bdca9619d03d5dbebdc967f1db
JustNrik <35231903+JustNrik@users.noreply.github.com>
2018-09-19 17:03:21 -0400

    Delete UnsupportedTypeException.vb

--- Exceptions/UnsupportedTypeException.vb
+++
@@ -1,7 +0,0 @@
-﻿Public Class UnsupportedTypeException
-    Inherits Exception
-    Private Const _defaul = "The object has a property with an unsupported type"
-    Sub New()
-        MyBase.New(_defaul)
-    End Sub
-End Class
