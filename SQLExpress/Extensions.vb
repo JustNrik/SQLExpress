afb407b7e17ee6eba47079810b65a8fda4424579
JustNrik <35231903+JustNrik@users.noreply.github.com>
2018-09-23 20:47:28 -0400

    1.0.7.1
    ToImmutableDictionary can infer it properly, idk why ToDictionary doesn't.

---
+++ SQLExpress/Extensions.vb
@@ -0,0 +1,8 @@
+﻿Imports System.Runtime.CompilerServices
+
+Friend Module Extensions
+    <Extension>
+    Friend Function ToDictionary(Of TKey, TValue)(dict As IEnumerable(Of KeyValuePair(Of TKey, TValue))) As IDictionary(Of TKey, TValue)
+        Return dict.ToDictionary(Function(x) x.Key, Function(x) x.Value)
+    End Function
+End Module
